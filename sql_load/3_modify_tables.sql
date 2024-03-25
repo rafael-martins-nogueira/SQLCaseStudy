741b7b65579aca0d2516b45ba37054bd14cb726a
rafael-martins-nogueira <rafael.mar.nog@gmail.com>
2024-03-25 00:43:16 +0000

    first commit

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -40,7 +40,3 @@ WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 COPY skills_job_dim
 FROM 'C:\Users\RafaelNogueira\OneDrive - Forward Forever Oy\Desktop\SQL COURSE\csv_files\skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-
-SELECT *
-FROM company_dim
